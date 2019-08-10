f36531e868efccf328ce8911b89fe637041893f2
Bryan Tremblay <bryanandhallie@gmail.com>
2019-08-10 10:38:53 -0400

    Some notes for Euler 76

--- Solutions/0076/Euler0076.py
+++ Solutions/0076/Euler0076.py
@@ -5,6 +5,10 @@
 
 # This might be helpful: https://www.math.upenn.edu/~wilf/PIMS/PIMSLectures.pdf
 
+# Looks like this is a known issue that seems to have no known closed form solution. This means I have to find other ways of calculating them and a few examples of how are outlined in this pdf:
+# https://pdfs.semanticscholar.org/df22/b005a381c7b837e781926589842dde8d8f80.pdf
+# I guess the problem being asked is 'calculate unrestricted partitions'
+
 e = 2
 fibmods = [1, 1]
 for n in range(10 ** e):
