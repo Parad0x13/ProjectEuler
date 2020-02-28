dfc31411ca035461f36803e927a198e57d59519d
Bryan Tremblay <bryanandhallie@gmail.com>
2020-02-28 15:07:11 -0500

    Minor changes

--- Solutions/0063_Solved.py
+++ Solutions/0063_Solved.py
@@ -1,21 +1,7 @@
 # Finished 2019.07.30.1530.EST
-# I totally guessed the max values for the ranges... but it worked!
 
 from Euler.Solution import Solution
 import Euler.Math as EM
-
-"""
-def logic():
-	count = 0
-	for x in range(1, 100):
-		for n in range(1, 10000):
-			val = pow(x, n)
-			length = len(str(val))
-			if length == n:
-				count += 1
-				print("#: {}^{} = {}, len = {}".format(count, x, n, val, length))
-	print("Done")
-"""
 
 def findDigitCountEqualToPower(power):
     retVal = []
