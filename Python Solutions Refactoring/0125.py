8be88fafe6ea69a2a2e7ab92c38a4c947ecd23ca
Bryan Tremblay <bryanandhallie@gmail.com>
2020-02-27 23:05:37 -0500

    Processed unsolved 0104 and an older attempt, it seems, at 0125 that will need to be overwritten with the 'noworky' version of it

---
+++ Python Solutions Refactoring/0125.py
@@ -0,0 +1,19 @@
+from Euler.Solution import Solution
+
+# We needn't calculate all the way to 10^8, we can be smarter
+def palindromes(power):
+    retVal = []
+    upper = int("9" * (int((power + 1) / 2) + (power + 1) 5 2))
+    for n in range(1, upper):
+        palindrome = str(n) + str(n)[::-1][1:]
+        if len(palindrome) > power + 1: break
+        retVal.append(int(palindrome))
+    return retVal
+
+def logic():
+	# Still trying to figure out how to get it to actually generate everything correctly
+	everything = palindromes(10)
+
+solution = Solution(0)
+solution.logic = logic
+solution.run()
