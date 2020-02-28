7d224e090533404df181dbe5dfc6133ea74f19aa
Bryan Tremblay <bryanandhallie@gmail.com>
2020-02-27 22:18:40 -0500

    Processed 0021

---
+++ Python Solutions Refactoring/0021_Solved.py
@@ -0,0 +1,28 @@
+from Euler.Solution import Solution
+
+def sumRange(theArray):
+    value = 0
+    for x in range(len(theArray)):
+        value += theArray[x]
+    return value
+
+def divisableArray(value):
+    theArray = []
+    for x in range(1, value):
+        if value % x == 0:
+            theArray.append(x)
+    return theArray
+
+def logic():
+	amicable = []
+
+	for x in range(1, 10000):
+		if not x == sumRange(divisableArray(x)):
+			if x == sumRange(divisableArray(sumRange(divisableArray(x)))):
+				amicable.append(x)
+
+	return sumRange(amicable)
+
+solution = Solution(31626)
+solution.logic = logic
+solution.run()
