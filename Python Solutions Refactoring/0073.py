939c6e1e1b59090152c39510fd0ae036ee4fef80
Bryan Tremblay <bryanandhallie@gmail.com>
2020-02-27 22:56:00 -0500

    Processed 0073 and 0076

---
+++ Python Solutions Refactoring/0073.py
@@ -0,0 +1,31 @@
+# Solved 2019.08.12.1655.EST as the 22405th person to do so
+
+from Euler.Solution import Solution
+
+# Uses Euclid's Method
+# [TODO] See if there's a faster way to optimize for larger values, e.g. testing if n is more than half of d etc...
+def HCF(n, d):
+    if n == 1: return False    # Small optimization for this puzzle, this is not mathematically correct however
+    while True:
+        if False: pass
+        elif n > d: n -= d
+        elif d > n: d -= n
+        elif n == d: return n
+
+def betweenTargets(D, targetL, targetR):
+    answers = 0
+    targetLValue = targetL[0] / targetL[1]
+    targetRValue = targetR[0] / targetR[1]
+    for d in range(1, D + 1):
+        for n in range(1, d):
+            value = n / d
+            if value > targetLValue and value > targetRValue and HCF(n, d) == 1:
+                answers += 1
+    return answers
+
+def logic():
+	return betweenTargets(12000, (1, 3), (1, 2))
+
+solution = Solution(7295372)
+solution.logic = logic
+solution.run()
