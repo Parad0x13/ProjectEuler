6de34fd22e6b13b35a577e31e219c4fc878ec551
Bryan Tremblay <bryanandhallie@gmail.com>
2020-02-28 00:30:35 -0500

    Processed 0031

--- Solutions/0031.py
+++ Solutions/0031.py
@@ -2,3 +2,12 @@
 # This means I have to find other ways of calculating them and a few examples of how are outlined in this pdf:
 # https://pdfs.semanticscholar.org/df22/b005a381c7b837e781926589842dde8d8f80.pdf
 # I guess the problem being asked is 'calculate unrestricted partitions'
+
+from Euler.Solution import Solution
+
+def logic():
+	pass
+
+solution = Solution(value = None, placement = None)
+solution.logic = logic
+solution.run()
