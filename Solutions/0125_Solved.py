d9e166214b1d7e669af1cf86abed3361e867380f
Bryan Tremblay <bryanandhallie@gmail.com>
2020-02-28 15:15:05 -0500

    Fixed bug with 0125

--- Solutions/0125_Solved.py
+++ Solutions/0125_Solved.py
@@ -5,24 +5,19 @@ import Euler.Math as EM
 
 values = []
 def palindromicConsecutiveSquares(base, limit):
-    global values
     n = base
     val = 0
     while True:
         val += n * n
         if val > limit: break
-
-        if str(val) == str(val)[::-1] and n - base > 0:
-            print("{} is palindromic with base {} to {}".format(val, base, n))
-            values.append(val)
-
+        if str(val) == str(val)[::-1] and n - base > 0: values.append(val)
         n += 1
 
 def logic():
-    limit = 10 ** 8    # [BUG] Something is wrong with the limit here that's causing these issues...
+    limit = 10 ** 8
     for n in range(1, limit):
         palindromicConsecutiveSquares(n, limit)
-    return sum(values)    # Turns out some values can be expressed as the sum of consecutive squares in multiple ways
+    return sum(set(values))    # Turns out some values can be expressed as the sum of consecutive squares in multiple ways
 
 solution = Solution(value = 2906969179, placement = 12032)
 solution.logic = logic
