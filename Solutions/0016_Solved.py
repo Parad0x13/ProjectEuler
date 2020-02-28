4e4f0b776c2efab80860ba9568f8c7ae436593a8
Bryan Tremblay <bryanandhallie@gmail.com>
2020-02-28 01:48:44 -0500

    Processed 0016

--- Solutions/0016_Solved.py
+++ Solutions/0016_Solved.py
@@ -1,10 +1,11 @@
 from Euler.Solution import Solution
+import Euler.Math as EM
 
 def logic():
 	exponential = 2 ** 1000
 	summed = str(exponential)
 	return sum(map(int, summed))
 
-solution = Solution(1366)
+solution = Solution(value = 1366, placement = None)
 solution.logic = logic
 solution.run()
