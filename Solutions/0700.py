7734cd7aaa2579edb058eb5ca9026c7d70258158
Bryan Tremblay <bryanandhallie@gmail.com>
2020-08-23 21:41:10 -0400

    Added more to 0700

--- Solutions/0700.py
+++ Solutions/0700.py
@@ -1,6 +1,8 @@
 from Euler.Solution import Solution
 import Euler.Math as EM
 
+import cProfile
+
 # There's gotta be a way of approximating the next n value to check instead of brute forcing it...
 # Or some underlying math thing I don't yet understand...
 
@@ -14,6 +16,9 @@ def logic():
 	foundNs = [1]
 	coins = [coin(1)]
 	n = 2
+	newDelta = 1
+	prevDelta = newDelta
+	#stillCoin = False
 	while True:
 		c = coin(n)
 		isCoin = True if c < coins[-1] else False
@@ -21,11 +26,23 @@ def logic():
 		if isCoin:
 			foundNs.append(n)
 			coins.append(c)
-			ratio = foundNs[-1] / foundNs[-2]    # Trying to find patterns...
-			print("{}\t\t{}\tsum {}\tdelta {}".format(n, c, sum(coins), ratio))
+			cDelta = coins[-2] - coins[-1]
+			#cDeltaRatio = coins[-2] / coins[-1]    # cDelta doesn't seem to be very helpful to reverse how much I should increase n by
+			nDelta = foundNs[-1] - foundNs[-2]
+			nDeltaRatio = nDelta / prevDelta
+			print("{} n:{}\t\tc:{}\tsum {}\t{:.2f}\tnDelta {} cDelta {}".format(len(coins), n, c, sum(coins), nDeltaRatio, nDelta, cDelta))
+			#print("{} n:{}\t\tc:{}\tsum {}\t{:.2f}\tnDelta {} cDelta {}".format(len(coins), n, c, sum(coins), cDeltaRatio, nDelta, cDelta))
 
-		n += 1
+			newDelta = nDelta    # Adjust the delta to try and skip to the next value, otherwise return newDelta back to 1 and keep seeking
+			prevDelta = newDelta
+			#stillCoin = True
+		else:
+			#if stillCoin: n = foundNs[-1] * 2    # This might only be a very small speedup...
+			#stillCoin = False
+			newDelta = 1
 
-solution = Solution(value = None, placement = None)
+		n += newDelta
+
+solution = Solution(value = None, placement = None, profile = True)
 solution.logic = logic
 solution.run()
