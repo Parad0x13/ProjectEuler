9598bb0df879b460ba6def38d63c427fce1cbf7a
Bryan Tremblay <bryanandhallie@gmail.com>
2023-07-03 15:21:04 -0400

    Working on problem 808 and I really think I should get the right solution but I don't. Pushing work now before I screw it all up

--- Solutions/0808.py
+++ Solutions/0808.py
@@ -4,19 +4,24 @@ import Euler.Math as EM
 # [NOTE] I really need to properly implement other primality tests... Pollard-Rho doesn't seem to work
 def logic():
     currentPrime = 1    # Yes I know 1 is not a prime...
-    for n in range(10):
+    foundPrimes = []
+    while True:
         currentPrime = EM.nextPrime(currentPrime)
         primeSquared = currentPrime ** 2
         reversedPrime = EM.reverseInt(primeSquared)
 
-        #if not EM.isSquare(reversedPrime): continue
+        if primeSquared == reversedPrime: continue    # We don't care about palindromes
+        if not EM.isSquare(reversedPrime): continue
 
-        print("The value {} squared is {} and reversed is {}".format(currentPrime, primeSquared, reversedPrime))
+        foundPrimes.append(primeSquared)
+
+        print("The prime {} squared is {} and reversed is {}. This makes {} found options".format(currentPrime, primeSquared, reversedPrime, len(foundPrimes)))
+
+        if len(foundPrimes) == 50: break
+
+    print("Found {} possibles".format(len(foundPrimes)))
+    print(sum(foundPrimes))
 
 solution = Solution(value = None, placement = None)
 solution.logic = logic
 solution.run()
-
-print()
-#print(EM.reverseInt(123))
-#print(str(123)[::-1])
