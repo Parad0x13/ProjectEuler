3d0ec96a1f01e9aea30bc237e2a4e24e7d683576
Bryan Tremblay <bryanandhallie@gmail.com>
2025-05-26 13:05:43 -0600

    Added a better way of calculating things without pre-allocating space!

--- Solutions/0918.py
+++ Solutions/0918.py
@@ -4,34 +4,43 @@ Wow... this one made my head swim at first!
 
 from Euler.Solution import Solution
 import Euler.Math as EM
+import math
 
 def logic():
-	N = 10 ** 2
-	#a = [1, 2, -5, 4, 17, -10, -17, 8, -47, 34]
-	a = [None] * (N + 1)    # This is NOT how it should be done, but I have to start somewhere. Zero all values up to some power of ten then calculate accordingly
-	#a[0] = 1
-	a[0] = 0
-	a[1] = 1
+	# Correct sequence will start off like: [1, 2, -5, 4, 17, -10, -17, 8, -47, 34]
+	N = 10 ** 1
 
-	for n in range(1, len(a)):
+	values = {}
+	values[1] = 1
+
+	#for n in range(1, N):
+	for n in range(1, int(N / 2) + 1):    # I think this is mathematically 'correct-ish' I just need to find a way to stop it exactly on the value we want
 		indexA = 2 * n
 		indexB = 2 * n + 1
 
 		try:
-			valA = 2 * a[n]
-			a[indexA] = valA
+			valA = 2 * values[n]
+			values[indexA] = valA
 		except:
 			pass
 
 		try:
-			valB = a[n] - (3 * a[n + 1])
-			a[indexB] = valB
+			valB = values[n] - (3 * values[n + 1])
+			values[indexB] = valB
 		except:
 			pass
 
-	print()
-	print(a)
-	print(sum(a))
+	print(values)
+
+	"""runningSum = 0
+	for n in range(1, N + 1):
+		runningSum += values[n]
+	print(runningSum)"""
+
+	runningSum = 0
+	for key in values.keys():
+		runningSum += values[key]
+	print(runningSum)
 
 solution = Solution(value = None, placement = None)
 solution.logic = logic
